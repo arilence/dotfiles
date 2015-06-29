a5f75e31dff5a575ecf5f7f8a55d66c8d4213a6a
Anthony Smith <anthony@smith.lt>
2015-06-29 13:39:23 -0700

    Organize files and add an Oh-My-Zsh environment file

--- install.sh
+++ install.sh
@@ -8,14 +8,19 @@
 ########################
 # Create symbolic links
 ########################
+# Make sure that our symlinks work
 CWD=$(pwd)
 
-ln -s vimrc ~/.vimrc
-ln -s zshrc ~/.zshrc
-ln -s vim/* ~/.vim/
-ln -s oh-my-zsh/alias.zsh ~/.oh-my-zsh/custom/
-ln -s oh-my-zsh/themes/ ~/.oh-my-zsh/custom/
-ln -s gitconfig ~/.gitconfig
+ln -s ${CWD}/vim/vimrc ~/.vimrc
+ln -s ${CWD}/vim/* ~/.vim/
+#rm ~/.vim/vimrc
+
+ln -s ${CWD}/oh-my-zsh/zshenv ~/.zshenv
+ln -s ${CWD}/oh-my-zsh/zshrc ~/.zshrc
+ln -s ${CWD}/oh-my-zsh/alias.zsh ~/.oh-my-zsh/custom/
+ln -s ${CWD}/oh-my-zsh/themes/ ~/.oh-my-zsh/custom/
+
+ln -s ${CWD}/git/gitconfig ~/.gitconfig
 
 # Because of limitations, we need to create the symbolic link a little
 # differently in side the application support folder
