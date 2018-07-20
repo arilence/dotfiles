c139348760a516536f4f6937c6de7263d52b6a8e
Kevin Chan <kchan.guoonho@gmail.com>
2018-07-20 09:06:26 -0700

    Trying Anthonys fix for symlink issue

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -3,11 +3,12 @@ source ./script/utils.sh
 
 e_header "Trying to configure Oh My Zsh..."
 
-mkdir -p ~/.oh-my-zsh/custom/
+rm -r ~/.oh-my-zsh/custom
+mkdir -p ~/.oh-my-zsh/custom
 
 CWD=$(pwd)
 ln -sf ${CWD}/oh-my-zsh/zshrc ~/.zshrc
-ln -sf ${CWD}/oh-my-zsh/custom/* ~/.oh-my-zsh/custom
+ln -sf ${CWD}/oh-my-zsh/custom/* ~/.oh-my-zsh/custom/
 
 if [ $? -ne 0 ]; then
     e_error "Configuration failed!"
