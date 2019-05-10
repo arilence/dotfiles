f98698d145dd0b9d7c0323ea2b636c0a145ea307
Anthony Smith <anthony@smith.cm>
2019-05-10 00:44:42 -0700

    Explicitly set install.sh files
    Before: Loop through every folder for install.sh
    After: Explicitly set which install.sh files to run
    
    This is for two reasons:
    - Allows for specifying order of module installation
    - Avoid potential random bugs in the future.

--- start.sh
+++ start.sh
@@ -4,11 +4,21 @@ if [[ $OSTYPE =~ "darwin" ]]; then
 # Ask for the administrator password upfront
 sudo -v
 
-./script/bootstrap.sh
+source ./script/bootstrap.sh
 
 # Make sure that the bootstrap succeeded before trying to install the rest
 if [ $? -ne 1 ]; then
-    ./script/install-all.sh
+  source ./alacritty/install.sh
+  source ./editorconfig/install.sh
+  source ./git/install.sh
+  source ./gnupg/install.sh
+  source ./python/install.sh
+  source ./tmux/install.sh
+  source ./vim/install.sh
+  source ./zsh/install.sh
+  source ./dock/install.sh
+  source ./osx/install.sh
+  e_success "Dotfiles has finished installing."
 fi
 
 else
