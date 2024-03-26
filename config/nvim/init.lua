9bb0d4f47d20362fe8dcae55f1afc9e8ba9fa847
Anthony Smith <anthony@smith.cm>
2024-03-25 22:23:19 -0700

    Disable nextls and only rely on credo for elixir

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -204,7 +204,7 @@ require("lazy").setup({
       local elixirls = require("elixir.elixirls")
 
       elixir.setup {
-        nextls = { enable = true },
+        nextls = { enable = false },
         credo = { enable = true },
         elixirls = {
           enable = true,
