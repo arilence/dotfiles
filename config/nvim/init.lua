5b098d3627ddc7a2f6f1f95c6981f54f3f1d5d22
Anthony Smith <anthony@smith.cm>
2024-04-02 15:29:09 -0700

    Run mix format automatically on file save

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -228,6 +228,38 @@ require("lazy").setup({
     },
   },
 
+  -- Runs code formatter tools on save
+  {
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+      local slow_format_filetypes = {}
+      require("conform").setup({
+        formatters_by_ft = {
+          elixir = { "mix" },
+        },
+        format_on_save = function(bufnr)
+          if slow_format_filetypes[vim.bo[bufnr].filetype] then
+            return
+          end
+          local function on_format(err)
+            if err and err:match("timeout$") then
+              slow_format_filetypes[vim.bo[bufnr].filetype] = true
+            end
+          end
+
+          return { timeout_ms = 200, lsp_fallback = true }, on_format
+        end,
+        format_after_save = function(bufnr)
+          if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+            return
+          end
+          return { lsp_fallback = true }
+        end,
+      })
+    end,
+  },
+
   -- File searching
   {
     "nvim-telescope/telescope.nvim", tag = "0.1.2",
