9ceea7c33eaf533eaac07928be1254795b61d71b
Anthony Smith <anthony@smith.cm>
2024-10-10 15:59:20 -0700

    feat: perform winget install during install.ps1

--- install.ps1
+++ install.ps1
@@ -30,8 +30,19 @@ $DOTBOT_BIN = "bin/dotbot"
 $BASEDIR = $PSScriptRoot
 
 Set-Location $BASEDIR
+git submodule update --recursive --remote
 git -C $DOTBOT_DIR submodule sync --quiet --recursive
 git submodule update --init --recursive $DOTBOT_DIR
+
+winget import --no-upgrade --accept-package-agreements --accept-source-agreements -i winget.json
+if ($LastExitCode -ne 0) {
+    Write-Error "Error: Winget install failed."
+    wait-before-exit
+    return
+}
+
+# Reload environment after installing applications with winget
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 foreach ($PYTHON in ('python', 'python3')) {
     # Python redirects to Microsoft Store in Windows 10 when not installed
