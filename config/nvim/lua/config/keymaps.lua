f89ffb5876b532e28cf38e2d1d23cca2ff8caccc
Anthony Smith <anthony@smith.cm>
2025-02-05 23:03:29 -0800

    feat(nvim): add options and keymaps from before lazyvim

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Clear current search with //
+vim.keymap.set("n", "//", ":nohlsearch <CR>")
+
+-- Stay in visual mode when indenting
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- Prevents (P)aste from overwriting value in buffer. This is handy when pasting a
+-- value to multiple locations.
+vim.keymap.set("v", "p", '"_dP')
--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Show a vertical line at the text column dictated by the value 'textwidth'
+vim.opt.colorcolumn = "+1"
+
+-- Show whitespace
+vim.opt.list = true
+vim.opt.listchars = "tab:▸ ,trail:."
+
+-- EditorConfig is enabled by default, but just in case
+vim.g.editorconfig = true
