fe51a647dc6a5728c4a040b98910c08136528e94
Anthony Smith <anthony@smith.cm>
2026-01-31 19:56:26 -0800

    fix: Remove nix warning about using ${system}

--- nix/machines/desktop/dev/elixir.nix
+++ nix/machines/desktop/dev/elixir.nix
@@ -7,6 +7,6 @@
 {
   environment.systemPackages = with pkgs; [
     inotify-tools
-    inputs.expert.packages.${pkgs.system}.default
+    inputs.expert.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 }
