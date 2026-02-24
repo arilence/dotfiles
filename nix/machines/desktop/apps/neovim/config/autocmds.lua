fd220af01673784ded166a6bb908098303266ea5
Anthony Smith <anthony@smith.cm>
2026-02-24 15:18:40 -0800

    chore: format lua file

--- nix/machines/desktop/apps/neovim/config/autocmds.lua
+++ nix/machines/desktop/apps/neovim/config/autocmds.lua
@@ -60,7 +60,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = args.buf,
       callback = function()
-        vim.lsp.buf.format {async = false, id = args.data.client_id }
+        vim.lsp.buf.format { async = false, id = args.data.client_id }
       end,
     })
   end
