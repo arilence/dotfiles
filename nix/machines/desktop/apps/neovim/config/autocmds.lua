5393596e90cab9bccb102a0bfc86ed9dc04f7dc6
Anthony Smith <anthony@smith.cm>
2026-02-24 15:17:58 -0800

    feat: Show diagnostics on cursor hover in neovim

--- nix/machines/desktop/apps/neovim/config/autocmds.lua
+++ nix/machines/desktop/apps/neovim/config/autocmds.lua
@@ -65,3 +65,27 @@ vim.api.nvim_create_autocmd("LspAttach", {
     })
   end
 })
+
+-- Show Diagnostics on Cursor Hover
+-- Credit: https://neovim.discourse.group/t/how-to-show-diagnostics-on-hover/3830/5
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+  pattern = "*",
+  callback = function()
+    for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+      if vim.api.nvim_win_get_config(winid).zindex then
+        return
+      end
+    end
+    vim.diagnostic.open_float({
+      scope = "cursor",
+      focusable = false,
+      close_events = {
+        "CursorMoved",
+        "CursorMovedI",
+        "BufHidden",
+        "InsertCharPre",
+        "WinLeave",
+      },
+    })
+  end
+})
