6bed6fd0f1f6800923363f5d77d219db60d356f1
Anthony Smith <anthony@smith.cm>
2026-02-25 17:17:43 -0800

    feat: Modify custom zellij zjstatus layout

--- nix/machines/desktop/apps/zellij.nix
+++ nix/machines/desktop/apps/zellij.nix
@@ -33,8 +33,24 @@
       initContent = lib.mkOrder 1500 ''
         export ZELLIJ_AUTO_EXIT=true
         eval "$(zellij setup --generate-auto-start zsh)"
+
+        # Dynamically rename zellij tabs
+        function _zellij_tab_cwd() {
+          [[ -n $ZELLIJ ]] && zellij action rename-tab "$(basename ''${PWD:l})"
+        }
+        function _zellij_tab_cmd() {
+          local cmd="''${1%% *}"
+          [[ -n $ZELLIJ ]] && zellij action rename-tab "''${cmd:l}"
+        }
+        add-zsh-hook chpwd _zellij_tab_cwd
+        add-zsh-hook precmd _zellij_tab_cwd
+        add-zsh-hook preexec _zellij_tab_cmd
       '';
     };
+
+    # Tell nix/home-manager to force create the config file even if one already exists.
+    # If you open zellij without a config file it will auto generate a default one and causes issues.
+    xdg.configFile."zellij/config.kdl".force = true;
 
     programs.zellij = {
       enable = true;
@@ -130,38 +146,50 @@
     };
 
     # Home Manager does have `programs.zellij.layouts` but I found it mangled the .kdl too much
-    xdg.configFile."zellij/layouts/default.kdl".text = ''
-      layout {
-        default_tab_template {
-          children
-          pane size=1 borderless=true {
-            plugin location="file:${pkgs.zjstatus}/bin/zjstatus.wasm" {
-              format_left   "{mode} #[fg=#89B4FA,bold]{session}"
-              format_center "{tabs}"
-              format_right  "{command_git_branch} {datetime}"
-              format_space  ""
+    xdg.configFile."zellij/layouts/default.kdl" = {
+      force = true;
+      # TODO: Add dark mode colours
+      text = ''
+        layout {
+          default_tab_template {
+            children
+            pane size=1 borderless=true {
+              plugin location="file:${pkgs.zjstatus}/bin/zjstatus.wasm" {
+                format_left   "{mode}#[fg=#6C6F85]"
+                format_center "{tabs}"
+                format_right  "{session} "
+                format_space  ""
 
-              border_enabled  "false"
-              border_char     "─"
-              border_format   "#[fg=#6C7086]{char}"
-              border_position "top"
+                border_enabled  "false"
+                border_char     "─"
+                border_format   "#[fg=#BCC0CC]{char}"
+                border_position "top"
 
-              hide_frame_for_single_pane "true"
+                hide_frame_for_single_pane "true"
 
-              mode_normal  "#[bg=blue] "
-              mode_tmux    "#[bg=#ffc387] "
+                mode_normal        "#[fg=#1E66F5] ● "
+                mode_locked        "#[fg=#D20F39,bold] locked "
+                mode_resize        "#[fg=#FE640B,bold] resize "
+                mode_pane          "#[fg=#40A02B,bold] pane "
+                mode_tab           "#[fg=#DF8E1D,bold] tab "
+                mode_scroll        "#[fg=#8839EF,bold] scroll "
+                mode_enter_search  "#[fg=#04A5E5,bold] search "
+                mode_search        "#[fg=#04A5E5,bold] search "
+                mode_rename_tab    "#[fg=#DF8E1D,bold] rename "
+                mode_rename_pane   "#[fg=#40A02B,bold] rename "
+                mode_session       "#[fg=#179299,bold] session "
+                mode_move          "#[fg=#DD7878,bold] move "
+                mode_prompt        "#[fg=#04A5E5,bold] prompt "
+                mode_tmux          "#[fg=#FE640B,bold] tmux "
 
-              tab_normal   "#[fg=#6C7086] {name} "
-              tab_active   "#[fg=#9399B2,bold,italic] {name} "
-
-              command_git_branch_command     "git rev-parse --abbrev-ref HEAD"
-              command_git_branch_format      "#[fg=blue] {stdout} "
-              command_git_branch_interval    "10"
-              command_git_branch_rendermode  "static"
+                tab_normal    "#[fg=#ACB0BE] {name} "
+                tab_active    "#[fg=#4C4F69,bold] {name} "
+                tab_separator "#[fg=#BCC0CC] • "
+              }
             }
           }
         }
-      }
-    '';
+      '';
+    };
   };
 }
