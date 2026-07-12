902d3f4f6cda47dec1630c3b8df475008100869b
Anthony Smith <anthony@smith.cm>
2026-07-12 11:37:22 -0700

    feat: add nix-community cachix substituter

--- nix/machines/desktop/configuration.nix
+++ nix/machines/desktop/configuration.nix
@@ -70,10 +70,12 @@ in
     extra-substituters = [
       "https://cache.numtide.com"
       "https://codex-desktop-linux.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       "codex-desktop-linux.cachix.org-1:nX/xy6AdK9hQE24A8ALGjkCKj2ObFmcnemiL5Cid4nk="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
