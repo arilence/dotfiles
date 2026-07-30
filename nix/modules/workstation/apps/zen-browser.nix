dd84ce8983bc0bef5c4cba40723f297ab947a1c5
Anthony Smith <anthony@smith.cm>
2026-07-30 00:18:51 -0700

    feat(zen): declaratively set keybinds

--- nix/modules/workstation/apps/zen-browser.nix
+++ nix/modules/workstation/apps/zen-browser.nix
@@ -137,6 +137,24 @@
             "zen.urlbar.behavior" = "float";
             "zen.mediacontrols.enabled" = false;
           };
+          # Find shortcut IDs in ~/.config/zen/default/zen-keyboard-shortcuts.json
+          # Get version from about:config -> zen.keyboard.shortcuts.version
+          # Activation fails if version changes (prevents silent breakage).
+          #
+          # Use this command:
+          # jq -c '.shortcuts[] | {id, key, keycode, action}' ~/.config/zen/default/zen-keyboard-shortcuts.json | fzf
+          keyboardShortcuts = [
+            {
+              id = "zen-compact-mode-toggle";
+              key = "[";
+              modifiers = {
+                accel = true;
+              };
+            }
+          ];
+          # In order to avoid breaking changes here, sometimes when you upgrade you should be asked
+          # to bump this version
+          keyboardShortcutsVersion = 19;
         };
 
         # HACK: This is a hack to add keybindings to the URL bar.
