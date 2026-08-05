0e0453710d9183e44e359bab3bcce39d8efc21c6
Anthony Smith <anthony@smith.cm>
2026-08-05 09:59:33 -0700

    feat(niri): open obsidian maximized

--- nix/modules/workstation/niri.nix
+++ nix/modules/workstation/niri.nix
@@ -144,6 +144,12 @@ in
       }
 
       window-rule {
+        match app-id="obsidian"
+        open-maximized true
+        open-focused true
+      }
+
+      window-rule {
         match app-id="steam" title=r#"^Steam$"#
         open-maximized true
       }
